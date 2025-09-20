c877d724bea18d66d5e97bb6edfe503c854df80b
valeriejuradoweb <valeriejuradoweb@gmail.com>
2025-09-20 17:32:11 +0200

    feat:Pushing more Jam card ridiculousness

--- src/components/JamCardScene.tsx
+++ src/components/JamCardScene.tsx
@@ -15,7 +15,7 @@ export default function JamCardScene({ hovered }: JamCardSceneProps) {
       <div className="relative w-full h-full">
         {/* Left stack */}
         <div className="absolute bottom-4 left-8 flex flex-col gap-0 z-[5] jam-stagger">
-          <div className="relative w-[150px] md:w-[200px] h-[25px] -mt-6">
+          <div className="relative w-[100px] md:w-[170px] h-[25px] -mt-6">
             <div className="absolute inset-0 card-overlay-loop">
               <img
                 src="/images/home-jam-card/ToDo1.png"
@@ -25,7 +25,7 @@ export default function JamCardScene({ hovered }: JamCardSceneProps) {
               />
             </div>
           </div>
-          <div className="relative w-[150px] md:w-[200px] h-[25px] -mt-6">
+          <div className="relative w-[100px] md:w-[170px] h-[25px] -mt-6">
             <div className="absolute inset-0 card-overlay-loop">
               <img
                 src="/images/home-jam-card/ToDo2.png"
@@ -35,7 +35,7 @@ export default function JamCardScene({ hovered }: JamCardSceneProps) {
               />
             </div>
           </div>
-          <div className="relative w-[150px] md:w-[200px] h-[25px] -mt-6">
+          <div className="relative w-[100px] md:w-[170px] h-[25px] -mt-6">
             <div className="absolute inset-0 card-overlay-loop">
               <img
                 src="/images/home-jam-card/ToDo3.png"
@@ -45,7 +45,7 @@ export default function JamCardScene({ hovered }: JamCardSceneProps) {
               />
             </div>
           </div>
-          <div className="relative w-[150px] md:w-[200px] h-[25px] -mt-6">
+          <div className="relative w-[100px] md:w-[170px] h-[25px] -mt-6">
             <div className="absolute inset-0 card-overlay-loop">
               <img
                 src="/images/home-jam-card/ToDo4.png"
@@ -55,7 +55,7 @@ export default function JamCardScene({ hovered }: JamCardSceneProps) {
               />
             </div>
           </div>
-          <div className="relative w-[150px] md:w-[200px] h-[25px] -mt-6">
+          <div className="relative w-[100px] md:w-[170px] h-[25px] -mt-6">
             <div className="absolute inset-0 card-overlay-loop">
               <img
                 src="/images/home-jam-card/ToDo5.png"
@@ -65,7 +65,7 @@ export default function JamCardScene({ hovered }: JamCardSceneProps) {
               />
             </div>
           </div>
-          <div className="relative w-[150px] md:w-[200px] h-[25px] -mt-6">
+          <div className="relative w-[100px] md:w-[170px] h-[25px] -mt-6">
             <div className="absolute inset-0 card-overlay-loop">
               <img
                 src="/images/home-jam-card/ToDo6.png"
@@ -83,14 +83,14 @@ export default function JamCardScene({ hovered }: JamCardSceneProps) {
         <img
           src="/images/home-jam-mobile.png"
           alt=""
-          className={`absolute left-1/2 -translate-x-1/2 -bottom-6 z-[10] h-[100%] w-auto object-contain drop-shadow-md transition-transform duration-300 ease-out transform-gpu ${hovered ? "-translate-y-[30px] scale-110" : "translate-y-0"}`}
+          className={`absolute left-1/2 -translate-x-1/2 -bottom-12 z-[10] h-[100%] w-auto object-contain drop-shadow-md transition-transform duration-300 ease-out transform-gpu ${hovered ? "-translate-y-[30px] scale-110" : "translate-y-0"}`}
           loading="lazy"
         />
 
         {/* Right stack */}
         <div className="absolute bottom-6 right-6 flex flex-col gap-0 z-[5] jam-stagger-right">
           
-          <div className="relative w-[180px] h-[40px]">
+          <div className="relative md:w-[180px] w-[100px] h-[40px]">
             <div className="absolute inset-0 card-overlay-loop -mt-6">
               <img
                 src="/images/home-jam-card/Schedule2.png"
@@ -100,7 +100,7 @@ export default function JamCardScene({ hovered }: JamCardSceneProps) {
               />
             </div>
           </div>
-          <div className="relative w-[180px] h-[40px]">
+          <div className="relative md:w-[180px] w-[100px] h-[40px]">
             <div className="absolute inset-0 card-overlay-loop -mt-6">
               <img
                 src="/images/home-jam-card/Schedule3.png"
@@ -110,7 +110,7 @@ export default function JamCardScene({ hovered }: JamCardSceneProps) {
               />
             </div>
           </div>
-          <div className="relative w-[180px] h-[30px]">
+          <div className="relative md:w-[180px] w-[100px] h-[30px]">
             <div className="absolute inset-0 card-overlay-loop -mt-6">
               <img
                 src="/images/home-jam-card/Schedule4.png"
@@ -120,7 +120,7 @@ export default function JamCardScene({ hovered }: JamCardSceneProps) {
               />
             </div>
           </div>
-          <div className="relative w-[180px] h-[40px]">
+          <div className="relative md:w-[180px] w-[100px] h-[40px]">
             <div className="absolute inset-0 card-overlay-loop -mt-6">
               <img
                 src="/images/home-jam-card/Schedule5.png"
@@ -130,7 +130,7 @@ export default function JamCardScene({ hovered }: JamCardSceneProps) {
               />
             </div>
           </div>
-          <div className="relative w-[180px] h-[30px]">
+          <div className="relative md:w-[180px] w-[100px] h-[30px]">
             <div className="absolute inset-0 card-overlay-loop -mt-6">
               <img
                 src="/images/home-jam-card/Schedule6.png"
